b82aea397d207b6194b63aa67ce268f9e787f3fb
Natali <natalikrivorozhenkop12@gmail.com>
2021-01-12 16:53:15 +0200

    feat: implement prototype of accordion with expansion pannels and fields, scroll to first invalid field + open expansion pannel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, QueryList, ViewChildren } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MatExpansionPanel } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'expansion-panel';
+  constructor(private readonly formBuilder: FormBuilder) {}
+  form = this.formBuilder.group({
+    name: ['', [Validators.required]],
+    age: ['', [Validators.required]],
+    surname: [''],
+    date: [''],
+  });
+
+  @ViewChildren(MatExpansionPanel) panels: QueryList<MatExpansionPanel>;
+  checkForm(): void {
+    this.panels.forEach((x, index) => {
+      if (index === 0 && this.form.get('name').invalid) {
+        x.open();
+        const formInvalid = document.getElementsByClassName('ng-invalid')[1];
+        formInvalid.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
